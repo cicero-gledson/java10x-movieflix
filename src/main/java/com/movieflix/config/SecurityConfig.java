3bf3440e096bea264a2cff0b48c82fcffbe34867
Cicero Gledson <cicero.gledson@hotmail.com>
2026-04-21 16:55:42 -0300

    refactor: altera o nome da classe SegurityConfig para SecurityConfig

--- src/main/java/com/movieflix/config/SecurityConfig.java
+++ src/main/java/com/movieflix/config/SecurityConfig.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SegurityConfig {
+public class SecurityConfig {
 
 //    private final SecurityFilter securityFilter;
 
